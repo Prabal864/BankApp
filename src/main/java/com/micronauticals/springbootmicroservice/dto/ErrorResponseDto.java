348f4ff53b73d1308f210b60af2bc621c95ee939
prabal864 <940pps@gmail.com>
2025-02-20 15:45:17 +0530

    Global Exception Handler

--- src/main/java/com/micronauticals/springbootmicroservice/dto/ErrorResponseDto.java
+++ src/main/java/com/micronauticals/springbootmicroservice/dto/ErrorResponseDto.java
@@ -1,12 +1,19 @@
 package com.micronauticals.springbootmicroservice.dto;
 
+import lombok.*;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
-public class errorResponseDto {
+@Data
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponseDto {
     private String apiPath;
     private HttpStatus errorCode;
     private String errorMessage;
     private LocalDateTime errorTime;
+
 }
--- src/main/java/com/micronauticals/springbootmicroservice/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/micronauticals/springbootmicroservice/exceptions/GlobalExceptionHandler.java
@@ -1,8 +1,30 @@
 package com.micronauticals.springbootmicroservice.exceptions;
+import com.micronauticals.springbootmicroservice.dto.ErrorResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
+import java.time.LocalDateTime;
 
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private final WebRequest webRequest;
+
+    public GlobalExceptionHandler(WebRequest webRequest) {
+        this.webRequest = webRequest;
+    }
+
+    public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(CustomerAlreadyExistsException exception) {
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.BAD_REQUEST,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
+    }
+
 }
