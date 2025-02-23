df7fdab1b66808a063896881308a3f5fc59d5ca0
prabal864 <940pps@gmail.com>
2025-02-23 11:44:08 +0530

    Adding Validations

--- src/main/java/com/micronauticals/springbootmicroservice/controllers/AccountsController.java
+++ src/main/java/com/micronauticals/springbootmicroservice/controllers/AccountsController.java
@@ -3,22 +3,26 @@ import com.micronauticals.springbootmicroservice.constants.AccountsConstants;
 import com.micronauticals.springbootmicroservice.dto.CustomerDto;
 import com.micronauticals.springbootmicroservice.dto.ResponseDto;
 import com.micronauticals.springbootmicroservice.service.IAccountsService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
+@Validated
 public class AccountsController {
 
     private IAccountsService iAccountsService;
 
     @PostMapping("/createAccount")
-   public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto ){
+   public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto ){
         iAccountsService.createAccount(customerDto);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
@@ -34,7 +38,7 @@ public class AccountsController {
    }
 
    @PutMapping("/updateAccount")
-    public ResponseEntity<ResponseDto> updateAccountDetails(@RequestBody CustomerDto customerDto){
+    public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto){
         boolean isUpdated = iAccountsService.updateAccount(customerDto);
         if(isUpdated){
             return ResponseEntity
@@ -48,7 +52,8 @@ public class AccountsController {
    }
 
    @DeleteMapping("/deleteAccount")
-    public ResponseEntity<ResponseDto> deleteAccount(@RequestParam String mobileNumber){
+    public ResponseEntity<ResponseDto> deleteAccount(@RequestParam @Pattern(regexp = "(^$|[0-9{10}])", message = "invalid Mobile number")
+                                                        String mobileNumber){
         boolean isDeleted = iAccountsService.deleteAccount(mobileNumber);
         if(isDeleted){
             return ResponseEntity
