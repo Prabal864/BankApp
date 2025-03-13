180695fd4f37134f0797bf497cb49aa9bfba9228
prabal864 <940pps@gmail.com>
2025-03-13 16:40:51 +0530

    Revert "Refreshing Configurations At Runtime"
    This reverts commit 2661f7765ab79391222109173a09a26476db3102.

--- V2_Cloud_Config/LoansMicroservice/src/main/java/com/micronauticals/loansmicroservice/LoansmicroserviceApplication.java
+++ V2_Cloud_Config/LoansMicroservice/src/main/java/com/micronauticals/loansmicroservice/LoansmicroserviceApplication.java
@@ -1,13 +1,10 @@
 package com.micronauticals.loansmicroservice;
 
-import com.micronauticals.loansmicroservice.dto.LoansContactInfoDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableConfigurationProperties(LoansContactInfoDto.class)
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 public class LoansmicroserviceApplication {
 
--- V2_Cloud_Config/cards_microservice/src/main/java/com/micronauticals/cardsmicroservice/CardsMicroserviceApplication.java
+++ V2_Cloud_Config/cards_microservice/src/main/java/com/micronauticals/cardsmicroservice/CardsMicroserviceApplication.java
@@ -1,6 +1,5 @@
 package com.micronauticals.cards_microservice;
 
-import com.micronauticals.cardsmicroservice.dto.CardsContactInfoDto;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -8,7 +7,6 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -36,7 +34,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
                 url = "https://www.eazybytes.com/swagger-ui.html"
         )
 )
-@EnableConfigurationProperties(CardsContactInfoDto.class)
 public class CardsMicroserviceApplication {
 
     public static void main(String[] args) {
