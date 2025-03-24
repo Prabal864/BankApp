2929f3e936858da23948329d1c8232d2460ca022
prabal864 <940pps@gmail.com>
2025-03-24 10:51:27 +0530

    Adding Feigns client

--- LoansMicroservice/src/main/java/com/micronauticals/loansmicroservice/LoansmicroserviceApplication.java
+++ LoansMicroservice/src/main/java/com/micronauticals/loansmicroservice/LoansmicroserviceApplication.java
@@ -4,9 +4,11 @@ import com.micronauticals.loansmicroservice.dto.LoansContactInfoDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableFeignClients
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 @EnableConfigurationProperties(value = LoansContactInfoDto.class)
 public class LoansmicroserviceApplication {
