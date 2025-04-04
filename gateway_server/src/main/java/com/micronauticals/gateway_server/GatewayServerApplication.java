3e9cac6a435ef1e6553efbe121c90a9f66f945cf
prabal864 <940pps@gmail.com>
2025-04-04 11:58:54 +0530

    Changing Default Setting in Circuit Breaker GWS

--- gateway_server/src/main/java/com/micronauticals/gateway_server/GatewayServerApplication.java
+++ gateway_server/src/main/java/com/micronauticals/gateway_server/GatewayServerApplication.java
@@ -1,7 +1,12 @@
 package com.micronauticals.gateway_server;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -24,7 +29,8 @@ public class GatewayServerApplication {
                         .path("/bank/accounts/**")
                         .filters(f -> f.rewritePath("/bank/accounts/(?<segment>.*)", "/${segment}")
                                 .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
-                        )
+                                .circuitBreaker(config -> config.setName("accountsCircuitBreaker")
+                                        .setFallbackUri("forward:/contactSupport")))
 
                         .uri("lb://ACCOUNTS"))
                 .route(p -> p
@@ -42,4 +48,12 @@ public class GatewayServerApplication {
                                 .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
                         .uri("lb://CARDS")).build();
     }
+
+    @Bean
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer(){
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
+    }
+
 }
