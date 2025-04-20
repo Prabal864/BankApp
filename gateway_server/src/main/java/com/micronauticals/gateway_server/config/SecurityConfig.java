0362963baf25799cb0f029461202422c1fefe30e
prabal864 <940pps@gmail.com>
2025-04-20 08:46:25 +0530

    Adding Role Based Access via KeyCloak Auth Server

--- gateway_server/src/main/java/com/micronauticals/gateway_server/config/SecurityConfig.java
+++ gateway_server/src/main/java/com/micronauticals/gateway_server/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -23,13 +22,18 @@ public class SecurityConfig {
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity.authorizeExchange(exchanges -> exchanges
                         .pathMatchers(HttpMethod.GET).permitAll()
-                        .pathMatchers("/bank/accounts/**").authenticated()
-                        .pathMatchers("/bank/cards/**").authenticated()
-                        .pathMatchers("/bank/loans/**").authenticated())
+                        .pathMatchers("/bank/accounts/**").hasRole("ACCOUNTS")
+                        .pathMatchers("/bank/cards/**").hasRole("CARDS")
+                        .pathMatchers("/bank/loans/**").hasRole("LOANS"))
                 .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
-                        .jwt(Customizer.withDefaults()));
+                        .jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter((grantedAuthoritiesExtractor()))));
         serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable);
         return serverHttpSecurity.build();
     }
 
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor(){
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeyCloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+    }
 }
